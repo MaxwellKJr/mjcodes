5e0dcada28debd966a060cc2de8b23e3ab91bfe0
Maxwell Kapezi Jr <maxwellkjr@gmail.com>
2021-07-27 15:54:44 -0700

    Styled the 404 Page

--- src/pages/404.js
+++ src/pages/404.js
@@ -9,8 +9,10 @@ const NotFoundPage = ({ data, location }) => {
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <div className="container center">
+        <h1>Page Not Found 😢</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      </div>
     </Layout>
   )
 }
