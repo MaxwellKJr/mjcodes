bd4e406493b51dba855a2f812794eb550e2e488a
Maxwell Kapezi Jr <maxwellkjr@gmail.com>
2021-07-27 01:25:46 -0700

    Updated all Gatsby Plugins

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,7 +55,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-172299875-2`,
       },
     },
     `gatsby-plugin-feed`,
